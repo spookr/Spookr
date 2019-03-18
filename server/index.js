22afa9c89cb77a2e53c97031dc60e1a3f54d5ed0
jordanutz <jordanutz@outlook.com>
2019-03-18 12:41:29 -0600

    Database and server now running

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
 const massive = require('massive')
 const session = require('express-session')
 const aws = require('aws-sdk');
-const auth = require('./controllers/auth_controller');
 const bcrypt = require('bcryptjs')
+const axios = require('axios')
+const path = require('path')
+
+// Controllers
+const auth = require('./controllers/auth_controller');
 
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY} = process.env
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json())
 
 app.use(
     session({
@@ -20,11 +26,15 @@ app.use(
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db);
-    app.listen(SERVER_PORT, () => console.log(`Now arriving at ${SERVER_PORT}`));
+    console.log('Database is spookin')
 })
 
-//account handling
+// Auth
 
 
 
-//when account is logged in
+// EndPoints
+
+app.listen(SERVER_PORT, ()=> {
+  console.log(`Spooking on Port ${SERVER_PORT} 👻 `)
+})
