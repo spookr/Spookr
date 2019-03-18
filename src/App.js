1037b8cfc56c99abe87ef7e596de1188a4de4097
Seth Gray <sethgray26@yahoo.com>
2019-03-18 09:52:09 -0600

    seths h3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import dabbing_ghost from './Pictures/dabbing_ghost.png'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='stuffs'>
+          <img src={dabbing_ghost}></img>
+        </div>
         <h1>Mark's h1</h1>
         <h2>Jordan's h2</h2>
         <h3>Seth's h3</h3>
