d654d1abf0fd51ac475dad4ff0f9e8f7edcbbb0f
Seth Gray <sethgray26@yahoo.com>
2019-03-18 09:53:20 -0600

    connecting to master

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className='stuffs'>
-          <img src={dabbing_ghost}></img>
-        </div>
         <h1>Mark's h1</h1>
         <h2>Jordan's h2</h2>
         <h3>Seth's h3</h3>
