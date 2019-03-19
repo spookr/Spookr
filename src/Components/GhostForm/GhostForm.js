ffc76fb9b6d9ced5ca6e56d91d3c663c519ca1b2
jordanutz <jordanutz@outlook.com>
2019-03-19 15:43:33 -0600

    All changes saved

--- src/Components/GhostForm/GhostForm.js
+++ src/Components/GhostForm/GhostForm.js
@@ -50,15 +50,13 @@ class GhostForm extends Component {
     })
   }
 
-
-
   render () {
 
     const {name, bio, gender, type, location} = this.state
     const {handleInput, handleDropdown, handleToggleMale, handleToggleFemale} = this
 
-    console.log('type', type)
-    console.log('gender', gender)
+    // console.log('type', type)
+    // console.log('gender', gender)
 
     return (
       <form>
@@ -71,14 +69,14 @@ class GhostForm extends Component {
           <Dropdown.Item name="gender" type="false" onClick={handleToggleFemale}>Female</Dropdown.Item>
         </DropdownButton>
 
-        <DropdownButton id="dropdown-basic-button" title={type == "1" ? "Poltergeist" :
-                                                          type == "2" ? "Demon" :
-                                                          type == "3" ? "Ectoplasm" :
-                                                          type == "4" ? "Interactive" :
-                                                          type == "5" ? "Orb" :
-                                                          type == "6" ? "Sheet" :
-                                                          type == "7" ? "Specter" :
-                                                          type == "8" ? "Other" : "Type"}>
+        <DropdownButton id="dropdown-basic-button" title={type === "1" ? "Poltergeist" :
+                                                          type === "2" ? "Demon" :
+                                                          type === "3" ? "Ectoplasm" :
+                                                          type === "4" ? "Interactive" :
+                                                          type === "5" ? "Orb" :
+                                                          type === "6" ? "Sheet" :
+                                                          type === "7" ? "Specter" :
+                                                          type === "8" ? "Other" : "Type"}>
           <Dropdown.Item name="type" type="1" onClick={(e) => handleDropdown(e)}>Poltergeist</Dropdown.Item>
           <Dropdown.Item name="type" type="2" onClick={(e) => handleDropdown(e)}>Demon</Dropdown.Item>
           <Dropdown.Item name="type" type="3" onClick={(e) => handleDropdown(e)}>Ectoplasm</Dropdown.Item>
@@ -90,7 +88,7 @@ class GhostForm extends Component {
         </DropdownButton>
 
         <h2>Location:</h2>
-        <input name="location" name="location" type="text" value={location} onChange={(e) => handleInput(e)}/>
+        <input name="location" type="text" value={location} onChange={(e) => handleInput(e)}/>
 
       </form>
     )
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -2,60 +2,37 @@ import React, { Component } from 'react'
 import './Navigation.scss'
 // Packages
 import axios from 'axios'
-<<<<<<< HEAD
-import { connect } from 'react-redux'
-import { logIn, logOut } from '../../redux/reducer'
-import { Link } from 'react-router-dom'
-import { withRouter } from 'react-router-dom'
-
+import {connect} from 'react-redux'
+import {logIn, logOut} from '../../redux/reducer'
+import {Link} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 
 class Navigation extends Component {
   componentDidMount() {
-=======
-import {connect} from 'react-redux'
-import {logIn} from '../../redux/reducer'
-import {Link} from 'react-router-dom'
-import {withRouter} from 'react-router-dom'
-class Navigation extends Component {
-  componentDidMount () {
->>>>>>> master
     axios.get('/api/user').then(res => {
       this.props.logIn(res.data)
     })
   }
-<<<<<<< HEAD
 
   logout = () => {
-    axios
-      .post("/logout")
-      .then(res => {
-        this.props.logOut();
-        this.props.history.push("/");
-      })
-      .catch(err => {
+    axios.post("/logout").then(res => {
+      this.props.logOut();
+      this.props.history.push("/");
+    }).catch(err => {
         console.log(err);
       });
-  };
+    };
 
 
   render() {
     const displayLogin = this.props.user ? <button onClick={this.logout}>Logout</button> : <Link to='/login'><button>Login</button></Link>
-=======
-  render () {
-    const displayLogin = this.props.user ? <button>Logout</button> : <Link to='/login'><button>Login</button></Link>
->>>>>>> master
     const displayLogo = this.props.user ? <Link to='/profile' style={{ textDecoration: 'none' }}><h1>Spookr</h1></Link> :
       <Link to='/' style={{ textDecoration: 'none' }}><h1>Spookr</h1></Link>
     return (
       <nav>
         <div className="NavigationSecondary">
-<<<<<<< HEAD
           {displayLogo}
           {displayLogin}
-=======
-        {displayLogo}
-        {displayLogin}
->>>>>>> master
         </div>
       </nav>
     )
@@ -70,8 +47,5 @@ const mapDispatchToProps = {
   logIn,
   logOut
 }
-<<<<<<< HEAD
+
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navigation))
-=======
-export default connect(mapStateToProps, mapDispatchToProps)(Navigation)
->>>>>>> master
