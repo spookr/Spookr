d05ebb6133d725cf7b7e19a99c3144b722addd0a
Savannah Cook <savannahcook@Savannahs-MacBook-Pro.local>
2019-03-20 10:18:29 -0600

    S3 backend implemented

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -6,27 +6,28 @@ import {connect} from 'react-redux'
 import {logIn, logOut} from '../../redux/reducer'
 import {Link} from 'react-router-dom'
 import {withRouter} from 'react-router-dom'
-
 class Navigation extends Component {
   componentDidMount() {
+    this.getUser()
+  }
+  componentDidUpdate (prevState, prevProps) {
+    if (prevState.user !== this.props.user) {
+      this.getUser()
+    }
+  }
+  getUser = () => {
     axios.get('/api/user').then(res => {
       this.props.logIn(res.data)
     })
   }
-
   logout = () => {
-    axios
-      .post("/logout")
-      .then(res => {
-        this.props.logOut();
-        this.props.history.push("/");
-      })
-      .catch(err => {
+    axios.post("/logout").then(res => {
+      this.props.logOut();
+      this.props.history.push("/");
+    }).catch(err => {
         console.log(err);
       });
-  };
-
-
+    };
   render() {
     const displayLogin = this.props.user ? <button onClick={this.logout}>Logout</button> : <Link to='/login'><button>Login</button></Link>
     const displayLogo = this.props.user ? <Link to='/profile' style={{ textDecoration: 'none' }}><h1>Spookr</h1></Link> :
