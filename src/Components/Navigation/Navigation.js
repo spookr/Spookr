d92436e7e2e33b1bf508eb1e867cd5b191400e73
Seth Gray <sethgray26@yahoo.com>
2019-03-20 10:56:47 -0600

    fixing some merges

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -2,28 +2,32 @@ import React, { Component } from 'react'
 import './Navigation.scss'
 // Packages
 import axios from 'axios'
-import {connect} from 'react-redux'
-import {logIn, logOut} from '../../redux/reducer'
-import {Link} from 'react-router-dom'
-import {withRouter} from 'react-router-dom'
-
+import { connect } from 'react-redux'
+import { logIn, logOut } from '../../redux/reducer'
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 class Navigation extends Component {
   componentDidMount() {
+    this.getUser()
+  }
+  componentDidUpdate(prevState, prevProps) {
+    if (prevState.user !== this.props.user) {
+      this.getUser()
+    }
+  }
+  getUser = () => {
     axios.get('/api/user').then(res => {
       this.props.logIn(res.data)
     })
   }
-
   logout = () => {
     axios.post("/logout").then(res => {
       this.props.logOut();
       this.props.history.push("/");
     }).catch(err => {
-        console.log(err);
-      });
-    };
-
-
+      console.log(err);
+    });
+  };
   render() {
     const displayLogin = this.props.user ? <button onClick={this.logout}>Logout</button> : <Link to='/login'><button>Login</button></Link>
     const displayLogo = this.props.user ? <Link to='/profile' style={{ textDecoration: 'none' }}><h1>Spookr</h1></Link> :
@@ -47,5 +51,4 @@ const mapDispatchToProps = {
   logIn,
   logOut
 }
-
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navigation))
