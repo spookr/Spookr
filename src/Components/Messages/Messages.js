0a16b71638fcdf0c1ea3bd4b043af3988da6d8ad
Seth Gray <sethgray26@yahoo.com>
2019-04-02 16:34:55 -0600

    basic setup for the socket rooms and messages

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,20 +1,22 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './Messages.scss'
 
 // Components
 import MessageCard from '../MessageCard/MessageCard'
 
 class Messages extends Component {
-  render () {
+  render() {
 
-    // <MessageCard toggleConversation={this.props.toggleConversation}/>
+    // <MessageCard toggleConversation={this.props.toggleConversation} />
 
     const displayMatches = this.props.matches && this.props.matches.length === 0 ?
-    <h2 style={{color: 'black'}}>You have no matches. Start swiping!</h2> : null
+      <h2 style={{ color: 'black' }}>You have no matches. Start swiping!</h2> : null
 
     return (
       <div className="Messages">
         <h1>Messages</h1>
+        <MessageCard toggleConversation={this.props.toggleConversation} />
+
         {displayMatches}
       </div>
     )
